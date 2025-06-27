11f822cafdc5bcafe74a58f26705cd952472f1d8
Thanapon Visedsang <thanapon.v@epic-consulting.com>
2025-06-27 10:06:52 +0700

    New Featuer : Calculate SUM.

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const name = "thanapon";
 console.log('Hello,',name);
+
+let number_x = 1;
+let number_y = 2;
+function sum(x,y){
+    return x+y;
+}
+
+
+let data_sum = sum(1,5)
+console.log(data_sum)
